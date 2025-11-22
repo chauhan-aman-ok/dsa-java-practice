e27b054a53f621f3f3b09f2f0a9e84acbbc05cef
Aman-Chauhan-ok <chauhanaman9688@gmail.com>
2025-11-22 19:05:22 +0530

    Implement sliding window to find max subarray average

--- arrays/slidingwindow/problem2/MaxSubarrayAvg.java
+++ arrays/slidingwindow/problem2/MaxSubarrayAvg.java
@@ -1,1 +1,29 @@
+// Leetcode - 643
+// problem2 - find the maximum possible average of any contiguous subarray of size k.
+// topic - fixed-size sliding window
 
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        double currAvg = 0;
+        int currSum = 0;
+        double maxAvg = Integer.MIN_VALUE;
+
+        for (int l = 0; l < k; l++)
+            currSum += nums[l];
+        currAvg = (double) currSum / k;
+        // maxAvg = currAvg;
+        int i = 0, j = k - 1;
+
+        while (j < nums.length) {
+            if (maxAvg < currAvg)
+                maxAvg = currAvg;
+            i++;
+            j++;
+            if (j < nums.length) {
+                currSum = currSum - nums[i - 1] + nums[j];
+                currAvg = (double) currSum / k;
+            }
+        }
+        return maxAvg;
+    }
+}

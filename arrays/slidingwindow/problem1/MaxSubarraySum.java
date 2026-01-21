59a73302f823476a6a81e07ce29c33924d5a21f8
Aman-Chauhan-ok <chauhanaman9688@gmail.com>
2026-01-21 19:16:15 +0530

    Refactor subarraySum method for clarity and efficiency

--- arrays/slidingwindow/problem1/MaxSubarraySum.java
+++ arrays/slidingwindow/problem1/MaxSubarraySum.java
@@ -2,21 +2,20 @@ package arrays.slidingwindow;
 // problem1 - find the maximum possible sum of any contiguous subarray of size k.
 // topic - fixed-size sliding window
 public class subarraySum {
-    public static int subarraySum(int[] nums, int k) {
-        int currSum=0;
-        int maxSum=0;
-        for(int i=0;i<k;i++){
-            currSum+=nums[i];
-        }
-        int i=1;
+    public static int subarraySum(int[] a, int k) {
+        int sum=0;
+        int max=0;
+        for(int l=0;l<k;l++) sum+=a[l];
         int j=k;
-        while(j<nums.length){
-            maxSum=Math.max(maxSum,currSum+nums[j]);
-            currSum=currSum-nums[i-1]+nums[j];
-            i++;
-            j++;
+        int i=1;
+        max=sum;
+        while(j<a.length){
+            
+            sum=sum+a[j]-a[i-1];
+            max=Math.max(sum,max);
+            j++;i++;
         }
-        return maxSum;
+        return max;
     }
     public static void main(String[] args) {
         int[] a={1,0,0,1,2,3,0,1,1,0};
